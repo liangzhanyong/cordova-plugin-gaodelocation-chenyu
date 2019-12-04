a9cb91f87d38657a1f297ae1f62e7b4b6ce506d8
liangzy <liangzy>
2019-12-04 09:50:21 +0800

    添加通知提醒&原生网络请求

--- src/android/GaoDeLocation.java
+++ src/android/GaoDeLocation.java
@@ -14,13 +14,27 @@ import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
 import com.amap.api.location.AMapLocationClientOption.AMapLocationProtocol;
 import com.amap.api.location.AMapLocationListener;
+import com.zjft.location.R;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import static org.chromium.base.ContextUtils.getApplicationContext;
 
 public class GaoDeLocation extends CordovaPlugin {
     //声明AMapLocationClient类对象
@@ -30,8 +44,12 @@ public class GaoDeLocation extends CordovaPlugin {
 
     //权限申请码
     private static final int PERMISSION_REQUEST_CODE = 500;
-    private int interval = 2 * 1000;
+    private LocationParam locationParam = new LocationParam();
+    private static final int LOCATION_SUCCESS = 0x01;
 
+    private static final String NOTIFICATION_CHANNEL_NAME = "BackgroundLocation";
+    private NotificationManager notificationManager = null;
+    boolean isCreateChannel = false;
     /**
      * 需要进行检测的权限数组
      */
@@ -57,7 +75,9 @@ public class GaoDeLocation extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getCurrentPosition")) {
             cb = callbackContext;
-            interval = args.getJSONObject(0).getInt("interval");
+            locationParam.setInterval(args.getJSONObject(0).getInt("interval"));
+            locationParam.setLineNo(args.getJSONObject(0).getString("lineNo"));
+            locationParam.setLineName(args.getJSONObject(0).getString("lineName"));
             PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
             pluginResult.setKeepCallback(true);
             cb.sendPluginResult(pluginResult);
@@ -66,7 +86,10 @@ public class GaoDeLocation extends CordovaPlugin {
             }else{
                 this.getCurrentPosition();
             }
-
+            return true;
+        }
+        else if(action.equals("stopLocation")) {
+            this.stopLocation();
             return true;
         }
         return false;
@@ -111,7 +134,7 @@ public class GaoDeLocation extends CordovaPlugin {
         mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
         mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
         mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.setInterval(interval);//可选，设置定位间隔。默认为2秒
+        mOption.setInterval(locationParam.getInterval());//可选，设置定位间隔。默认为2秒
         mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
         mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
         mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
@@ -180,12 +203,45 @@ public class GaoDeLocation extends CordovaPlugin {
                 PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json);
                 pluginResult.setKeepCallback(true);
                 cb.sendPluginResult(pluginResult);
+
+                //启动后台定位，第一个参数为通知栏ID，建议整个APP使用一个
+                locationClient.enableBackgroundLocation(2001, buildNotification());
+                Message msg = new Message();
+                msg.what = LOCATION_SUCCESS;
+                msg.obj = json;
+                mHandler.sendMessage(msg);
             } catch (JSONException e) {
                 // PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, e.getMessage());
                 // pluginResult.setKeepCallback(true);
                 // cb.sendPluginResult(pluginResult);
             } finally {
                 // locationClient.stopLocation();
+            }
+        }
+    };
+
+    //新建Handler对象。
+    Handler mHandler = new Handler(){
+
+        //handleMessage为处理消息的方法
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == LOCATION_SUCCESS) {
+                try {
+                    Date date = new Date();
+                    JSONObject params = new JSONObject();
+                    JSONObject obj = (JSONObject)msg.obj;
+
+                    params.put("lineNo", locationParam.getLineNo());
+                    params.put("lineName", locationParam.getLineName());
+                    params.put("lng", obj.get("longitude"));
+                    params.put("lat", obj.get("latitude"));
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    params.put("locationDate", dateFormat.format(date).substring(0, 10));
+                    params.put("locationTime", dateFormat.format(date).substring(11));
+                    String result = HttpUtil.doPost("http://171.221.175.230:8070/iTMS/visible/cartrace/insertLocation", params.toString());
+                    Log.i("HTTP RESULT", result);
+                } catch(Exception e) {}
             }
         }
     };
@@ -224,6 +280,11 @@ public class GaoDeLocation extends CordovaPlugin {
         }
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.destroyLocation();
+    }
 
     /**
      * 判断是否需要检查权限
@@ -283,6 +344,76 @@ public class GaoDeLocation extends CordovaPlugin {
                                           String[] permissions, int[] paramArrayOfInt) {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             this.getCurrentPosition();
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private Notification buildNotification() {
+
+        Notification.Builder builder = null;
+        Notification notification = null;
+        if(android.os.Build.VERSION.SDK_INT >= 26) {
+            //Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
+            if (null == notificationManager) {
+                notificationManager = (NotificationManager) cordova.getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            String channelId = cordova.getActivity().getPackageName();
+            if(!isCreateChannel) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,
+                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_MIN);
+                notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
+                notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
+                notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+                notificationManager.createNotificationChannel(notificationChannel);
+                isCreateChannel = true;
+            }
+            builder = new Notification.Builder(cordova.getActivity().getApplicationContext(), channelId);
+        } else {
+            builder = new Notification.Builder(cordova.getActivity().getApplicationContext());
+        }
+        builder.setSmallIcon(R.mipmap.icon)
+                .setContentTitle("智慧金库")
+                .setContentText("正在后台运行")
+                .setWhen(System.currentTimeMillis());
+
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = builder.build();
+        } else {
+            return builder.getNotification();
+        }
+        return notification;
+    }
+
+    class LocationParam {
+        private int interval = 2 * 1000;
+        private String lineNo;
+        private String lineName;
+
+        public LocationParam() {
+        }
+
+        public int getInterval() {
+            return interval;
+        }
+
+        public void setInterval(int interval) {
+            this.interval = interval;
+        }
+
+        public String getLineNo() {
+            return lineNo;
+        }
+
+        public void setLineNo(String lineNo) {
+            this.lineNo = lineNo;
+        }
+
+        public String getLineName() {
+            return lineName;
+        }
+
+        public void setLineName(String lineName) {
+            this.lineName = lineName;
         }
     }
 }
--- www/GaoDeLocation.js
+++ www/GaoDeLocation.js
@@ -3,6 +3,10 @@ var exec = require('cordova/exec');
 var GaoDe = {
     getCurrentPosition:function (options, successFn, errorFn) {
         exec(successFn,errorFn,'GaoDeLocation','getCurrentPosition',[options]);
+    },
+
+    stopLocation:function (successFn, errorFn) {
+        exec(successFn, errorFn, 'GaoDeLocation', 'stopLocation', []);
     }
 };
 
